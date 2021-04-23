b442f283ad655af636295014d14afb2afd8faaeb
Dell <zhao.hu@dataxgroup.com>
2021-04-23 13:35:08 +0800

    commit

---
+++ spring-cloud-eureka-server-singleton/src/main/java/AuD/eureka/server/SingletonEurekaServer.java
@@ -0,0 +1,21 @@
+package AuD.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: TODO
+ *
+ * @author AuD/胡钊
+ * @ClassName SingletonEurekaServer
+ * @date 2021/4/23 12:19
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class SingletonEurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingletonEurekaServer.class,args);
+    }
+
+}
